6e6be7de326558f6b359de12aa37512b1d3535c3
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2018-05-05 10:38:56 +0200

    Refactoring

--- lib/generic_job.rb
+++ lib/generic_job.rb
@@ -1,38 +1,12 @@
 # frozen_string_literal: true
 
 class GenericJob < ActiveJob::Base
-  def perform obj_or_hash, meth
-    case obj_or_hash
-    when Hash
-      handle_passed_hash obj_or_hash.symbolize_keys, meth
-    else
-      handle_passed_obj obj_or_hash, meth
-    end
-  end
-
-  private
-
-    def init_obj hash
-      hash[:class].constantize.new hash[:init_args]
-    end
-
-    def handle_passed_hash hash, meth
-      if hash[:meth_args]
-        init_obj(hash).send meth, hash[:meth_args]
-      else
-        init_obj(hash).send meth
-      end
-    end
-
-    def handle_passed_obj obj, data
-      case data
-      when String
-        obj.send data
-      when Hash
-        obj.send data[:meth], data[:arg]
-      end
-    end
 end
 
-require 'generic_job/stub'
-require 'generic_job/async'
+require_relative 'generic_job/perform'
+require_relative 'generic_job/stub'
+require_relative 'generic_job/async'
+
+class GenericJob
+  include GenericJob::Perform
+end
---
+++ lib/generic_job/perform.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class GenericJob
+  module Perform
+    def perform obj_or_hash, meth
+      case obj_or_hash
+      when Hash
+        handle_passed_hash obj_or_hash.symbolize_keys, meth
+      else
+        handle_passed_obj obj_or_hash, meth
+      end
+    end
+
+    private
+
+      def init_obj hash
+        hash[:class].constantize.new hash[:init_args]
+      end
+
+      def handle_passed_hash hash, meth
+        if hash[:meth_args]
+          init_obj(hash).send meth, hash[:meth_args]
+        else
+          init_obj(hash).send meth
+        end
+      end
+
+      def handle_passed_obj obj, data
+        case data
+        when String
+          obj.send data
+        when Hash
+          obj.send data[:meth], data[:arg]
+        end
+      end
+  end
+end
