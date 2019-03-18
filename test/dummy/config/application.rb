2406e57ad892d49c31ac0a4c0b7b0abe0cca0708
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-03-18 23:23:55 +0100

    Cleaning

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -2,18 +2,12 @@
 
 require_relative 'boot'
 
-require 'rails'
-# Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-# require "active_storage/engine"
-require 'action_controller/railtie'
-# require "action_mailer/railtie"
-require 'action_view/railtie'
-# require "action_cable/engine"
-# require "sprockets/railtie"
-require 'rails/test_unit/railtie'
+%w[
+  active_job/railtie
+  active_record/railtie
+].each do |railtie|
+  require railtie
+end
 
 Bundler.require(*Rails.groups)
 require 'generic_job'
